ed28c9475b59a00a826f387c3df44966fb793b45
fajarartbekasi <fajarbekasieditting@gmail.com>
2022-03-05 08:21:07 +0700

    change url upload payment in button mail

--- resources/views/email/pembelian.blade.php
+++ resources/views/email/pembelian.blade.php
@@ -5,7 +5,7 @@
     upload pembayaran anda terimakasih
 </p>
 <P>Kode Invoice : {{$sale->invoice}}</P>
-@component('mail::button', ['url' => "http://askes.test/user/ambil-form/{$sale->id}"])
+@component('mail::button', ['url' => "http://askes.xyz/user/ambil-form/{$sale->id}"])
 upload pembayaran
 @endcomponent
 @endcomponent
